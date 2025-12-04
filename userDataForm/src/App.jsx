f36a5024110df7e148c517684f1c3282022848ac
Nicola <nic00la.kaleta@gmail.com>
2025-12-04 18:52:10 +0100

    first commit

---
+++ userDataForm/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <form>
+          <label>Imię: </label>
+          <input type='text'/>
+          <label>Nazwisko: </label>
+          <input type='text'/>
+          <label>E-Mail: </label>
+          <input type='text'/>
+          <button>Przejdź do kolejnego formularza</button>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default App
